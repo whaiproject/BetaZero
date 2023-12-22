58142219fbdf895af01d0b06d8056b56f16fdf80
lbarazza <leonardo.barazza@gmail.com>
2023-12-22 18:58:05 +0100

    Introduced training loop and basic SL network

--- src/tic_tac_toe/SL/train.py
+++ src/tic_tac_toe/SL/train.py
@@ -1,29 +1,189 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
 import torch
 from torch.utils.data import Dataset, DataLoader
-import pandas as pd
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import random_split
+import itertools
 
 class TicTacToeDataset(Dataset):
     def __init__(self, csv_file, state_shape, action_shape):
-        self.dataframe = pd.read_csv(csv_file)
+        self.original_dataframe = pd.read_csv(csv_file)
         self.state_shape = state_shape
         self.action_shape = action_shape
-    
+        self.dataframe = self.expand_with_transforms()
+
+    def tensor_to_str(self, tensor):
+        return ','.join(map(str, tensor.reshape(-1).tolist()))
+
+    def expand_with_transforms(self):
+        unique_data = set()
+        expanded_data = []
+        transforms = [
+            lambda x: x,  # No transformation
+            lambda x: x.flip(dims=[0]),  # Flip vertically
+            lambda x: x.flip(dims=[1]),  # Flip horizontally
+            lambda x: x.rot90(1, [0, 1]),  # Rotate 90 degrees
+            lambda x: x.rot90(2, [0, 1]),  # Rotate 180 degrees
+            lambda x: x.rot90(3, [0, 1]),  # Rotate 270 degrees
+            lambda x: x.t(),  # Transpose (flip along one diagonal)
+            lambda x: x.flip(dims=[0]).t(),  # Flip around the other diagonal
+        ]
+
+        for _, row in self.original_dataframe.iterrows():
+            state = torch.tensor([float(x) for x in row['State'].split()], dtype=torch.float).view(self.state_shape)
+            action = torch.tensor([float(x) for x in row['Action'].split()], dtype=torch.float).view(self.action_shape)
+            for transform in transforms:
+                transformed_state = transform(state)
+                transformed_action = transform(action)
+
+                # Check for uniqueness
+                state_str = self.tensor_to_str(transformed_state)
+                action_str = self.tensor_to_str(transformed_action)
+                if (state_str, action_str) not in unique_data:
+                    unique_data.add((state_str, action_str))
+                    expanded_data.append((transformed_state, transformed_action))
+
+        return expanded_data
+
     def __len__(self):
         return len(self.dataframe)
-    
+
     def __getitem__(self, idx):
-        row = self.dataframe.iloc[idx]
-        state = torch.tensor([float(x) for x in row['State'].split()], dtype=torch.float).view(self.state_shape)
-        action = torch.tensor([float(x) for x in row['Action'].split()], dtype=torch.float).view(self.action_shape)
-        return state, action
+        state, action = self.dataframe[idx]
+        return state.clone(), action.clone()
 
 # Example usage
+# dataset = TicTacToeDataset('tic_tac_toe_data.csv', (3, 3), (3, 3))
+# state, action = dataset[0]
+
+
+
+# # Example usage
+# state_shape = (3, 3)  # Example for a 3x3 Tic Tac Toe board
+# action_shape = (3, 3) # actions are also represented in a 3x3 structure
+# dataset = TicTacToeDataset('tic_tac_toe_data.csv', state_shape, action_shape)
+# dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+# for states, actions in dataloader:
+#     print(states)
+#     print(actions)
+#     break
+
+class TicTacToeNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(TicTacToeNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
 state_shape = (3, 3)  # Example for a 3x3 Tic Tac Toe board
 action_shape = (3, 3) # actions are also represented in a 3x3 structure
-dataset = TicTacToeDataset('tic_tac_toe_data.csv', state_shape, action_shape)
-dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
 
-for states, actions in dataloader:
-    print(states)
-    print(actions)
-    break
+# Parameters
+validation_split = 0.2  # Percentage of data for validation
+batch_size = 128
+val_batch_size = 64
+
+# Create the full dataset
+full_dataset = TicTacToeDataset('tic_tac_toe_data.csv', state_shape, action_shape)
+
+# Splitting dataset into train and validation
+total_size = len(full_dataset)
+val_size = int(total_size * validation_split)
+train_size = total_size - val_size
+
+train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+
+# Creating data loaders for both sets
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=val_batch_size, shuffle=False)
+
+def train_model(model, train_loader, val_loader, epochs, learning_rate):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Lists to store loss values
+    train_losses = []
+    val_losses = []
+
+    plt.ion()  # Turn on interactive mode
+    fig, ax = plt.subplots(figsize=(10, 5))
+
+    for epoch in range(epochs):
+        # Training phase
+        model.train()
+        train_loss = 0.0
+        for states, actions in train_loader:
+            states = states.view(states.size(0), -1)
+            actions = actions.view(actions.size(0), -1).argmax(dim=1)
+
+            optimizer.zero_grad()
+            outputs = model(states)
+            loss = criterion(outputs, actions)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+
+        # Validation phase
+        model.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for states, actions in val_loader:
+                states = states.view(states.size(0), -1)
+                actions = actions.view(actions.size(0), -1).argmax(dim=1)
+
+                outputs = model(states)
+                loss = criterion(outputs, actions)
+                val_loss += loss.item()
+
+        # Calculate average losses
+        train_loss_avg = train_loss / len(train_loader)
+        val_loss_avg = val_loss / len(val_loader)
+
+        train_losses.append(train_loss_avg)
+        val_losses.append(val_loss_avg)
+
+        # Plotting
+        ax.clear()
+        ax.plot(train_losses, label='Training Loss')
+        ax.plot(val_losses, label='Validation Loss')
+        ax.set_xlabel('Epochs')
+        ax.set_ylabel('Loss')
+        ax.set_title('Training and Validation Loss')
+        ax.legend()
+        plt.draw()
+        plt.pause(0.001)
+
+        print(f'Epoch {epoch+1}, Training Loss: {train_losses[-1]}, Validation Loss: {val_losses[-1]}')
+
+    plt.ioff()  # Turn off interactive mode
+    plt.show()
+
+# Example usage
+input_size = 9  # 3x3 Tic Tac Toe board flattened
+hidden_size = 20 #64  # Example size of hidden layer
+# with 64 neurons it wasn't able to generalize well on the validation set
+# with 20 it's better
+output_size = 9  # Assuming 3x3 action space
+
+model = TicTacToeNet(input_size, hidden_size, output_size)
+
+# Assuming you have created 'dataloader' from TicTacToeDataset
+epochs = 1000  # Number of epochs to train
+learning_rate = 0.0001  # Learning rate
+
+print("Length of train dataset: ", len(train_dataset))
+print("Length of val dataset: ", len(val_dataset))
+print("Training model...")
+train_model(model, train_loader, val_loader, epochs, learning_rate)
+print("Done!")
