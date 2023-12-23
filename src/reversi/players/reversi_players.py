24cb5a509d0a2f9310be5026e24469e8ec40464a
pedrou2000 <urbina2000pedro@gmail.com>
2023-12-23 15:39:57 +0100

    changed to matrix action encoding

--- src/reversi/players/reversi_players.py
+++ src/reversi/players/reversi_players.py
@@ -1,5 +1,12 @@
-from abc import ABC, abstractmethod
+import sys
+from pathlib import Path
+parent_dir = str(Path(__file__).resolve().parent.parent)
+sys.path.append(parent_dir)
+
+import numpy as np
 import random
+from abc import ABC, abstractmethod
+from game_logic.reversi_board import ReversiBoard
 
 class ReversiPlayer(ABC):
     @abstractmethod
@@ -16,7 +23,9 @@ class HumanPlayer(ReversiPlayer):
                 row = int(input(f"Enter the row (0-{board.size-1}): "))
                 col = int(input(f"Enter the column (0-{board.size-1}): "))
                 if 0 <= row < board.size and 0 <= col < board.size and board.is_valid_move(row, col, self.symbol):
-                    return row, col
+                    action_matrix = np.zeros((board.size, board.size), dtype=int)
+                    action_matrix[row, col] = 1
+                    return action_matrix
                 else:
                     print("Invalid move. Please enter a valid row and column within the board boundaries.")
             except ValueError:
@@ -27,12 +36,18 @@ class RandomPlayer(ReversiPlayer):
         self.symbol = symbol  # 1 for X, -1 for O
 
     def get_move(self, board):
-        import random
         moves = board.generate_possible_moves(self.symbol)
-        return random.choice(moves) if moves else (None, None)
+        if moves:
+            row, col = random.choice(moves)
+            action_matrix = np.zeros((board.size, board.size), dtype=int)
+            action_matrix[row, col] = 1
+            return action_matrix
+        else:
+            return np.zeros((board.size, board.size), dtype=int)
+
 
 # Optimal player
-class OptimalPlayer:
+class OptimalPlayer(ReversiPlayer):
     def __init__(self, symbol):
         self.symbol = symbol  # 1 for X, -1 for O
 
@@ -73,3 +88,15 @@ class OptimalPlayer:
                     best_score = score
                     best_move = move
             return best_score, best_move
+
+
+def main():
+    # Example game
+    board = ReversiBoard(size=4)
+    print(board)
+    player = HumanPlayer(1)
+    action = player.get_move(board)
+    print(action)
+
+if __name__ == "__main__":
+    main()
