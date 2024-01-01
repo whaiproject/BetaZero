f55b641a95ae79894cd18b2015cc8dffe4a8bf0d
lbarazza <leonardo.barazza@gmail.com>
2024-01-01 22:19:12 +0100

    Added MCTS, some bugs still to fix

--- src/main.py
+++ src/main.py
@@ -49,7 +49,6 @@ class MCTSNode:
             possible_moves = current_state.get_valid_moves()
             move = random.choice(possible_moves)
             current_state = current_state.make_move(move, current_player)
-            #print(current_state.board)
             current_player *= -1
 
         self.P = 1 # change this to be set to the output of the neural net
@@ -57,13 +56,7 @@ class MCTSNode:
 
     def backpropagate(self, result):
         current_node = self
-        #print("??"*20)
         while current_node is not None:
-            # print("+"*20)
-            # print(result)
-            # print(current_node.player)
-            # print(result * current_node.player)
-            # print("+"*20)
             current_node.N += 1
             # the - here is needed because the result is from the perspective of the
             # player who just moved which is the parent of the current node
@@ -71,71 +64,87 @@ class MCTSNode:
             current_node.Q = current_node.W / current_node.N
             current_node = current_node.parent
 
+    def find_child_by_move(self, move):
+        # Search through the children nodes for a node that corresponds to the given move.
+        # Returns the child node if found, otherwise returns None.
 
-def mcts(root_state, iterations, player=1):
-    root_node = MCTSNode(root_state, player=player)
-
-    # perform various simulations
-    for _ in range(iterations):
-        node = root_node.select()
-        if not node.game_state.is_game_over():
-            node.expand()
-
-        # continue with lightweight policy
-        result = node.evaluate()
-        node.backpropagate(result)
-
-    return root_node, max(root_node.children, key=lambda node: node.N).move
-
-
-
-# class RandomPlayer(Player):
-#     def get_move(self, board):
-#         return random.choice(board.get_valid_moves())
+        for child in self.children:
+            if child.move == move:
+                return child
+        return None
     
+    def find_child_by_state(self, game_state):
+        # Search through the children nodes for a node that corresponds to the given game state.
+        # Returns the child node if found, otherwise returns None.
+        
+        for child in self.children:
+            if np.allclose(child.game_state.board, game_state.board):
+                return child
+        return None
+
+    def mcts(self, iterations):
+
+        # perform various simulations
+        for _ in range(iterations):
+            node = self.select()
+            if not node.game_state.is_game_over():
+                node.expand()
+
+            # continue with lightweight policy
+            result = node.evaluate()
+            node.backpropagate(result)
+
+    def get_best_child(self):
+        # Returning the best move from this node's children
+        return max(self.children, key=lambda node: node.N)
+    
+    def get_Ns(self):
+        Ns = np.zeros((3, 3))
+        for node in self.children:
+            Ns[node.move] = node.N
+        return Ns
+
+    def get_Qs(self):
+        Qs = np.zeros((3, 3))
+        for node in self.children:
+            Qs[node.move] = node.Q
+        return Qs
+ 
 
 class BetaZeroPlayer(Player):
-    def __init__(self, player):
+    def __init__(self, player, verbose=False):
         super().__init__()
         self.player = player
+        self.root_node = None
+        self.simulations_per_turn = 1000
+        self.verbose = verbose
 
     def get_move(self, board):
-        root_node, move = mcts(board, 1000, player=self.player)
-        Ns = np.zeros((3, 3))
-        Qs = np.zeros((3, 3))
-        for node in root_node.children:
-            Ns[node.move] = node.N
-            Qs[node.move] = node.Q
-        print(Ns)
-        print(Qs)
-        print("#"*20)
+        if self.root_node is None:
+            self.root_node = MCTSNode(board, player=self.player)
+        else:
+            self.root_node = self.root_node.find_child_by_state(board)
+        self.root_node.mcts(1000)
+
+        best_child = self.root_node.get_best_child()
+        move = best_child.move
+
+        if self.verbose:
+            # Get Ns and Qs for debugging or analysis purposes
+            Ns = self.root_node.get_Ns()
+            Qs = self.root_node.get_Qs()
+            print(Ns)
+            print(Qs)
+            print("+"*45)
+        
+        self.root_node = best_child
+
         return move
 
 
-player1 = BetaZeroPlayer(1)
-player2 = RandomPlayer()
+player1 = RandomPlayer()
+player2 = BetaZeroPlayer(-1, verbose=True)
 
 # Initialize the terminal game manager
 terminal_game_manager = TicTacToeTerminal(player1, player2)
 terminal_game_manager.play()
-
-
-# board = TicTacToeBoard()
-# board = board.make_move((0, 0), 1)
-# board = board.make_move((0, 1), -1)
-# board = board.make_move((1, 1), 1)
-# board = board.make_move((2, 1), -1)
-
-
-# print(board.board)
-
-# root_node, a = mcts(board, 100, player=1)
-
-# #Ns = np.array([node.N for node in root_node.children]).reshape(3, 3)
-# Ns = np.zeros((3, 3))
-# for node in root_node.children:
-#     Ns[node.move] = node.N
-
-# print(root_node.N)
-# print(Ns)
-# print(a)
